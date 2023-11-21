d2b61de505d0f360f51f31ba79aa25675458aede
Kacper Golinski <kacper.golinski@gmail.com>
2023-11-21 17:38:25 +0100

    yet another update

--- run_martin.sh
+++ run_martin.sh
@@ -10,7 +10,6 @@ docker rm $(docker ps -a -q --filter ancestor=ghcr.io/maplibre/martin)
 # -d detached mode
 docker run \
   -d \
-  --rm \
   --restart unless-stopped \
   -p 127.0.0.1:3000:3000 \
   -v "$(pwd)/data-mbtiles":/data-mbtiles \
